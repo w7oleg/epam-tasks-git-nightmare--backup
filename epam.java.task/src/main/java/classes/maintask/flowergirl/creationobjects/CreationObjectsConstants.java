59b4a432ab46edfe6414b919b3556241858282be
Oleg Vostrykau <w7oleg@gmail.com>
2021-07-17 19:07:31 +0300

    putting things in order

--- epam.java.task/src/main/java/classes/maintask/flowergirl/creationobjects/CreationObjectsConstants.java
+++
@@ -1,6 +0,0 @@
-package classes.maintask.flowergirl.creationobjects;
-
-public interface CreationObjectsConstants {
-    public String FLOWER = "flower";
-    public String ACCESSORY = "accessory";
-}
--- epam.java.task/src/main/java/classes/maintask/flowergirl/creationobjects/bouqet/BouquetFactory.java
+++
@@ -1,7 +0,0 @@
-package classes.maintask.flowergirl.creationobjects.bouqet;
-
-public class BouquetFactory {
-    public static Bouquet getFlowerBouquet() {
-        return new FlowerBouquet();
-    }
-}
